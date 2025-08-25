519c4d139dadfbe57fcc0f48c35100cfb8d2f25b
Pabitra-33 <kumarpabitra345@gmail.com>
2025-08-25 20:10:44 +0530

    created a notification service interface to provide a notification send service

---
+++ notificationserviceapp/src/main/java/notificationservice/services/NotificationServices.java
@@ -0,0 +1,8 @@
+package notificationservice.services;
+
+public interface NotificationServices {
+	
+	//abstract method
+	//it defines the contract for sending notifications.
+	void sendNotification(String message);
+}
