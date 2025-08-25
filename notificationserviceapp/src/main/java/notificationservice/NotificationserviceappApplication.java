b25b4553276713044a0d355f2b8b8a7957e91a47
Pabitra-33 <kumarpabitra345@gmail.com>
2025-08-25 20:06:43 +0530

    initial project setup commit

---
+++ notificationserviceapp/src/main/java/notificationservice/NotificationserviceappApplication.java
@@ -0,0 +1,13 @@
+package notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationserviceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationserviceappApplication.class, args);
+	}
+
+}
